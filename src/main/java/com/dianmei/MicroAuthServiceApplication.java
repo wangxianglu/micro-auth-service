9abd53933e313e6bc6082c8a5433301ed5ff49d5
wangxianglu <lu_action@qq.com>
2017-04-11 22:53:57 +0800

    first commit

---
+++ src/main/java/com/dianmei/MicroAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dianmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAuthServiceApplication.class, args);
+	}
+}
