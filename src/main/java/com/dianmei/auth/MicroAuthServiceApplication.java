8ac6519813117da0232b722e47110bb41d988291
wangxianglu <lu_action@qq.com>
2017-04-17 18:17:51 +0800

    update

--- src/main/java/com/dianmei/auth/MicroAuthServiceApplication.java
+++ src/main/java/com/dianmei/auth/MicroAuthServiceApplication.java
@@ -1,61 +1,58 @@
 package com.dianmei.auth;
 
-import com.dianmei.auth.service.security.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-@EnableResourceServer
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MicroAuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroAuthServiceApplication.class, args);
 	}
 
-	@Configuration
 	@EnableWebSecurity
-	protected static class webSecurityConfig extends WebSecurityConfigurerAdapter{
-
-		@Autowired
-		private CustomUserDetailService customUserDetailService;
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-					.authorizeRequests().anyRequest().authenticated()
-					.and().csrf().disable();
-		}
+	@Configuration
+	protected static class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 		@Override
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(customUserDetailService)
-					.passwordEncoder(new BCryptPasswordEncoder());
+
+			auth.inMemoryAuthentication().withUser("wang").password("xianglu").roles("ADMIN");
 		}
 
-		@Bean
 		@Override
-		public AuthenticationManager authenticationManagerBean() throws Exception {
-			return super.authenticationManagerBean();
+		protected void configure(HttpSecurity http) throws Exception {
+			http.anonymous().disable()
+					.authorizeRequests()
+					.anyRequest().authenticated()
+				 .and()
+					.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		}
+
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/hystrix.stream/**", "/info", "/error");
 		}
 	}
 
@@ -63,16 +60,10 @@ public class MicroAuthServiceApplication {
 	@EnableAuthorizationServer
 	protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter{
 
-		private TokenStore tokenStore = new InMemoryTokenStore();
-
 		@Autowired
 		private AuthenticationManager authenticationManager;
 
-		@Autowired
-		private CustomUserDetailService customUserDetailService;
-
-		@Autowired
-		private Environment env;
+		private TokenStore tokenStore = new InMemoryTokenStore();
 
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -81,40 +72,21 @@ public class MicroAuthServiceApplication {
 
 			// @formatter:off
 			clients.inMemory()
-					.withClient("browser")
-					.authorizedGrantTypes("refresh_token", "password")
-					.scopes("ui")
-					.and()
-					.withClient("account-service")
-					.secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-					.scopes("server")
-					.and()
-					.withClient("statistics-service")
-					.secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-					.scopes("server")
-					.and()
-					.withClient("notification-service")
-					.secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
-					.authorizedGrantTypes("client_credentials", "refresh_token")
+					.withClient("notification")
+					.secret("admin")
+					.authorizedGrantTypes("authorization_code", "refresh_token")
 					.scopes("server");
 			// @formatter:on
 		}
 
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-			endpoints
-					.tokenStore(tokenStore)
-					.authenticationManager(authenticationManager)
-					.userDetailsService(customUserDetailService);
+			endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore);
 		}
 
 		@Override
 		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-			security
-					.tokenKeyAccess("permitAll()")
-					.checkTokenAccess("isAuthenticated()");
+			security.tokenKeyAccess("permitAll()");
 		}
 	}
 }
--- src/main/java/com/dianmei/auth/controller/UserController.java
+++ src/main/java/com/dianmei/auth/controller/UserController.java
@@ -4,6 +4,7 @@ import com.dianmei.auth.domain.User;
 import com.dianmei.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
+@EnableResourceServer
 public class UserController {
 
     @Autowired
