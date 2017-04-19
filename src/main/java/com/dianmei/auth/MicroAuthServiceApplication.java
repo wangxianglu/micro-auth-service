44ce274eaccf470a881ccc2d582f90afb45f6829
wangxianglu <lu_action@qq.com>
2017-04-19 17:55:46 +0800

    update applicationconfig

--- src/main/java/com/dianmei/auth/MicroAuthServiceApplication.java
+++ src/main/java/com/dianmei/auth/MicroAuthServiceApplication.java
@@ -3,57 +3,24 @@ package com.dianmei.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @SpringBootApplication
 public class MicroAuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroAuthServiceApplication.class, args);
-	}
-
-	@EnableWebSecurity
-	@Configuration
-	protected static class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-			auth.inMemoryAuthentication().withUser("wang").password("xianglu").roles("ADMIN");
-		}
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.anonymous().disable()
-					.authorizeRequests()
-					.anyRequest().authenticated()
-				 .and()
-					.sessionManagement()
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		}
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.ignoring().antMatchers("/hystrix.stream/**", "/info", "/error");
-		}
 	}
 
 	@Configuration
@@ -63,30 +30,41 @@ public class MicroAuthServiceApplication {
 		@Autowired
 		private AuthenticationManager authenticationManager;
 
-		private TokenStore tokenStore = new InMemoryTokenStore();
-
 		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-
-			// TODO persist clients details
-
-			// @formatter:off
-			clients.inMemory()
-					.withClient("notification")
-					.secret("admin")
-					.authorizedGrantTypes("authorization_code", "refresh_token")
-					.scopes("server");
-			// @formatter:on
+		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+			security.allowFormAuthenticationForClients();
 		}
 
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-			endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore);
+			endpoints.authenticationManager(authenticationManager);
 		}
 
 		@Override
-		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-			security.tokenKeyAccess("permitAll()");
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			clients.inMemory().withClient("foo").secret("foosecret")
+					.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
 		}
+	}
+
+
+	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+	protected static class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+		@Override
+		protected MethodSecurityExpressionHandler createExpressionHandler() {
+			System.out.println("MethodSecurityConfig :::::::::::");
+			return new OAuth2MethodSecurityExpressionHandler();
+		}
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.inMemoryAuthentication()
+				.withUser("marcelo").password("123").roles("admin")
+				.and()
+				.withUser("user1").password("123").roles("user");
+
 	}
 }
